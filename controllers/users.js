69a0ea6145ed9d06607ab39d994c25f76dba4655
Caglar Kaya <caglar.kaya.europe@gmail.com>
2022-01-08 16:30:02 +0100

    update controllers

--- controllers/users.js
+++ controllers/users.js
@@ -23,23 +23,38 @@ export const createUser = (req, res) => {
   const user = req.body;
   const userWithId = { id: uuidv4(), ...user };
   users.push(userWithId);
+  res.status(200);
   res.send(`User with the name ${user.firstName} added to the database!`);
 };
 
 export const getUsers = (req, res) => {
+  res.status(200);
   res.send(users);
 };
 
 export const getUser = (req, res) => {
   const { id } = req.params;
   const foundUser = users.find((user) => user.id === id);
-  res.send(foundUser);
+  if (foundUser) {
+    res.status(200);
+    res.send(foundUser);
+  } else {
+    res.status(404);
+    res.send(`The user with id ${id} not found in the database.`);
+  }
 };
 
 export const deleteUser = (req, res) => {
   const { id } = req.params;
-  users = users.filter((user) => user.id !== id);
-  res.send(`User with the id ${id} deleted from the database.`);
+  const userExist = users.some((user) => user.id === id);
+  if (userExist) {
+    users = users.filter((user) => user.id !== id);
+    res.status(200);
+    res.send(`User with the id ${id} deleted from the database.`);
+  } else {
+    res.status(404);
+    res.send(`The user with id ${id} not found in the database.`);
+  }
 };
 
 export const updateUser = (req, res) => {
@@ -48,11 +63,32 @@ export const updateUser = (req, res) => {
 
   const user = users.find((user) => user.id === id);
 
-  if (firstName) user.firstName = firstName;
-
-  if (lastName) user.lastName = lastName;
-
-  if (age) user.age = age;
-
-  res.send(`User with the id ${id} has been updated.`);
+  if (user) {
+    if (firstName) {
+      user.firstName = firstName;
+    } else {
+      res.status(400);
+      res.send(`The user needs a firstName property set.`);
+      return;
+    }
+    if (lastName) {
+      user.lastName = lastName;
+    } else {
+      res.status(400);
+      res.send(`The user needs a lastName property set.`);
+      return;
+    }
+    if (age) {
+      user.age = age;
+    } else {
+      res.status(400);
+      res.send(`The user needs an age property set.`);
+      return;
+    }
+    res.status(200);
+    res.send(`The user with id ${id} has been updated.`);
+  } else {
+    res.status(404);
+    res.send(`The user with id ${id} not found in the database.`);
+  }
 };
